1cd1f7a077a478e1ab64e65f0eacc84b1ac8d7d2
Vinicius Eduardo <vinimice42@gmail.com>
2022-11-28 22:40:35 -0300

    [Inicio] Iniciando projeto

---
+++ Duozin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Duozin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
