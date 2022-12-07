ec78261326a047fd02347c05bad03566b38cde86
Vinicius Eduardo <vinimice42@gmail.com>
2022-12-06 23:18:05 -0300

    [Adicao] Polindo layout e adicionando Controller

--- Duozin/wwwroot/js/index.js
+++ Duozin/wwwroot/js/index.js
@@ -7,3 +7,22 @@ function LutadorSelecionado(){
         card.classList.add("selected");
     }
 }
+
+
+let button = document.getElementById("backTop")
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
