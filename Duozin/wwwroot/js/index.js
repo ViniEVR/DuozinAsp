82288e827c4bf7886316d9c30ee7b9c2dd33d289
Vinicius Eduardo <vinimice42@gmail.com>
2022-12-13 15:16:20 -0300

    [Adicao] Adicionando imagem com checkbox

--- Duozin/wwwroot/js/index.js
+++ Duozin/wwwroot/js/index.js
@@ -1,13 +1,29 @@
 function LutadorSelecionado(id){
-    var card = document.getElementById(id);
-
+    let card = document.getElementById(id);
     if($(card).hasClass("selected")){
-        card.classList.remove("selected");
+        card.classList.remove("selected"); 
     } else {
-        card.classList.add("selected");
+        card.classList.add("selected");     
     }
 }
 
+function check(name) {
+    let checkBox = document.getElementById(name);
+
+    if(checkBox.checked == false){
+        checkBox.checked = true;
+    } else {
+        checkBox.checked = false;
+    }
+}
+
+function countCheckbox(){
+    let checks = document.getElementsByClassName("selected").length
+    if(checks > 16 || checks < 16){
+        window.location.reload();
+        alert('Problema')
+    }
+}
 
 let button = document.getElementById("backTop")
 
